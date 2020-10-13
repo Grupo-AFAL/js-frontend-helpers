2162bc95ea416478926cb3d39947ea9207a6663b
Federico Gonzalez <fedegl@hey.com>
2020-10-13 10:37:34 -0700

    Fix bug when burger is not present

--- src/controllers/navbar-controller.js
+++ src/controllers/navbar-controller.js
@@ -6,6 +6,9 @@ export default class NavbarController extends Controller {
   toggleMenu (event) {
     event.preventDefault()
     this.menuTarget.classList.toggle('is-active')
-    this.burgerTarget.classList.toggle('is-active')
+
+    if (this.hasBurgerTarget) {
+      this.burgerTarget.classList.toggle('is-active')
+    }
   }
 }
