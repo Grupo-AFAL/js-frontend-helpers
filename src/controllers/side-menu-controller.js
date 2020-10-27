ff3720df8ee020226e97ffae474baa05796e771d
Federico Gonzalez <fedegonzalez@afal.mx>
2020-10-27 19:44:37 +0000

    Handle case where overlay is null on side-menu-controller

--- src/controllers/side-menu-controller.js
+++ src/controllers/side-menu-controller.js
@@ -4,13 +4,17 @@ export default class SideMenuController extends Controller {
   static targets = ['container', 'overlay']
 
   connect () {
-    this.overlayTarget.addEventListener('click', this.closeMenu)
-    this.containerTarget.addEventListener('click', this.closeMenu)
+    if (this.hasOverlayTarget && this.hasContainerTarget) {
+      this.overlayTarget.addEventListener('click', this.closeMenu)
+      this.containerTarget.addEventListener('click', this.closeMenu)
+    }
   }
 
   disconnect () {
-    this.overlayTarget.removeEventListener('click', this.closeMenu)
-    this.containerTarget.removeEventListener('click', this.closeMenu)
+    if (this.hasOverlayTarget && this.hasContainerTarget) {
+      this.overlayTarget.removeEventListener('click', this.closeMenu)
+      this.containerTarget.removeEventListener('click', this.closeMenu)
+    }
   }
 
   toggleMenu (e) {
