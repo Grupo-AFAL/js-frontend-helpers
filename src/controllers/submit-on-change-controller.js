e6b4ba94b0af8c2b22a7bff0d345fee4b4a975b0
Alexis Gutiérrez <alxkinto@gmail.com>
2021-03-04 16:48:09 -0800

    Added navigator from hotwired (#4)
    * Added navigator from hotwired
    
    * Change to hotwired beta.5 and correct navigator method

--- src/controllers/submit-on-change-controller.js
+++ src/controllers/submit-on-change-controller.js
@@ -1,7 +1,8 @@
 import { Controller } from 'stimulus'
+import { navigator } from '@hotwired/turbo'
 
 export default class SubmitOnChangeController extends Controller {
   submit () {
-    this.element.submit()
+    navigator.submitForm(this.element)
   }
 }
